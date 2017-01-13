663ec38dfbb9b83939a8b5fe1f09420da914a164
Howell <th15817161961@gmail.com>
2017-01-13 15:39:24 +0800

    no message

--- config/mymaster/start.sh
+++ config/mymaster/start.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 cd /etc/redis/
-exec nohup redis-server /etc/redis/redis.conf > nohup.out1 2>&1&
+#6379
+exec nohup redis-server /etc/redis/redis_0.conf > nohup.out1 2>&1&
 
-exec docker-entrypoint.sh redis-server /etc/redis/sentinel.conf --sentinel
+#6380
+exec nohup redis-server /etc/redis/redis_1.conf > nohup.out2 2>&1&
+
+#6381
+exec nohup redis-server /etc/redis/redis_2.conf > nohup.out3 2>&1&
+
+#26381
+exec nohup docker-entrypoint.sh redis-server /etc/redis/sentinel_1.conf --sentinel  > nohup.out4 2>&1&
+
+#26380
+exec nohup docker-entrypoint.sh redis-server /etc/redis/sentinel_2.conf --sentinel  > nohup.out5 2>&1&
+
+#26379
+exec docker-entrypoint.sh redis-server /etc/redis/sentinel_0.conf --sentinel
